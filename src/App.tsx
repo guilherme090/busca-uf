cc6a2037de843691ec47536736ed2d65eb33818c
guilherme <g090simoes@gmail.com>
2025-10-20 21:44:52 -0300

    <feat> end exercise

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,50 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { buscaRegioes, Regiao, Estado, buscaEstados } from './Api';
 
 function App() {
+  const [regioes, setRegioes] = useState<Regiao[]>([]);
+  useEffect(() => {
+    buscaRegioes().then(r => setRegioes(r));
+  },[]);
+
+  const [idRegiao, setIdRegiao] = useState<string>("");
+
+  const [estados, setEstados] = useState<Estado[]>([]);
+  useEffect(() => {
+    buscaEstados(idRegiao).then(r => setEstados(r));
+  },[idRegiao]);
+
+  const [idUf, setIdUf] = useState<string>("");
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form>
+      <div>
+        <label htmlFor="regioes">Região: </label>
+        <select id="regioes" onChange={e => {
+            setIdRegiao(e.target.value)
+            setIdUf("");
+          }}>
+          <option value="">Selecione uma região</option>
+          {regioes.map(regiao => (
+            <option key={regiao.id} value={regiao.id}>{regiao.nome}</option>
+          ))}
+        </select>
+      </div>
+      <div>
+        <label htmlFor="estados">Estado: </label>
+        <select id="estados" onChange={e => setIdUf(e.target.value)}>
+          <option>Selecione um estado</option>
+          {estados.map(estado => (
+            <option key={estado.id} value={estado.id}>{estado.nome}</option>
+          ))}
+        </select>
+      </div>
+      <div>
+        Id do estado selecionado: {idUf}; 
+      </div>
+    </form>  
   );
 }
 
